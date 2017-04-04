1af42cde280db38ac79757444b836cc2e7eaf04a
Alex Kovar <ajkovar@gmail.com>
2017-04-04 11:49:33 -0500

    show more product info

--- lib/components/Product.jsx
+++ lib/components/Product.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import CSSModules from 'react-css-modules';
 import styles from './Product.css'
+import TimeAgo from 'react-timeago'
 
 function Product(props) {
   let pxSize = `${props.size}px`
@@ -9,7 +10,22 @@ function Product(props) {
     fontSize: pxSize,
     lineHeight: pxSize
   }
-  return <div style={style} styleName="product">{props.face}</div>
+  const date = new Date(props.date)
+  let sevenDaysAgo = new Date()
+  sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
+  let dateComponent = date < sevenDaysAgo ? <div>{props.date}</div> : <TimeAgo date={date} />
+  return (
+    <div styleName="product">
+      <div>
+        {props.price}
+      </div>
+      <div>
+      {dateComponent}
+      </div>
+      <div style={style} styleName="preview">
+        {props.face}
+      </div>
+    </div>)
 }
 
 export default CSSModules(Product, styles, {allowMultiple: true})
